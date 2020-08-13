51a75ef15c91b92485766a1d0e721bcbcab9d419
Ed Ignot <edignot@gmail.com>
2020-08-13 14:40:43 -0600

    Mongoose unique validator

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
@@ -10,4 +11,5 @@ const userSchema = new Schema({
     places: { type: String, required: true },
 });
 
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', userSchema);
